a65e2c98cd6b9b0dc3c452520e4ada82cc091f79
hzy <andrew_hzy@163.com>
2022-12-05 01:32:15 +0800

    add some strange code for trying

--- src/effectivejava/chapter6/item39/annotationwithparameter/Sample2.java
+++ src/effectivejava/chapter6/item39/annotationwithparameter/Sample2.java
@@ -5,16 +5,16 @@ import java.util.*;
 
 // Program containing annotations with a parameter (Page 183)
 public class Sample2 {
-    @ExceptionTest(value1 = ArithmeticException.class,value2 = Integer.class)
+    @ExceptionTest(value1 = ArithmeticException.class,value2 = Integer.class,integer = 1)
     public static void m1() {  // Test should pass
         int i = 0;
         i = i / i;
     }
-    @ExceptionTest(value1 = ArithmeticException.class,value2 = Integer.class)
+    @ExceptionTest(value1 = ArithmeticException.class,value2 = Integer.class,integer = 1)
     public static void m2() {  // Should fail (wrong exception)
         int[] a = new int[0];
         int i = a[1];
     }
-    @ExceptionTest(value1 = ArithmeticException.class,value2 = Integer.class)
+    @ExceptionTest(value1 = ArithmeticException.class,value2 = Integer.class,integer = 1)
     public static void m3() { }  // Should fail (no exception)
 }
